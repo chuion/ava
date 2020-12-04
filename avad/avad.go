3bd652ab7845f7360ff390e01dcf2c5ef18a0911
inksnw <inksnw@gmail.com>
2020-12-04 21:30:09 +0800

    连接各个结点

--- avad/avad.go
+++ avad/avad.go
@@ -1,76 +1,86 @@
 package avad
 
 import (
-	"flag"
-	"log"
-	"net/url"
-	"os"
-	"os/signal"
-	"time"
-
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
 )
 
-var addr = flag.String("addr", "localhost:8080", "http service address")
+var addrs = []string{"localhost:8080", "localhost:8081"}
+
+var allconns = make(map[string]*websocket.Conn)
+
+func DialOne(addr string) {
+
+	conned := false
+	for {
+		if !conned {
+			u := url.URL{Scheme: "ws", Host: addr, Path: "/echo"}
+			c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+			if err != nil {
+				log.Printf("尝试连接节点%s失败,3s后重试:\n", addr)
+				conned = false
+				time.Sleep(3 * time.Second)
+				continue
+			}
+			conned = true
+			allconns[addr] = c
+			fmt.Printf("已创建连接节点%s\n", addr)
+		}
+
+	}
+}
 
 func DLocal() {
-	flag.Parse()
-	log.SetFlags(0)
+	for _, addr := range addrs {
+		go DialOne(addr)
+	}
+	http.HandleFunc("/start", handel)
+	addr := "localhost:4000"
+	log.Fatal(http.ListenAndServe(addr, nil))
 
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
+}
 
-	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
-	log.Printf("connecting to %s", u.String())
+type Task struct {
+	Cmd  string
+	Args string
+}
 
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+type rusult struct {
+	Code int
+	Msg  string
+}
+
+func handel(w http.ResponseWriter, r *http.Request) {
+	var p Task
+	err := json.NewDecoder(r.Body).Decode(&p)
 	if err != nil {
-		log.Fatal("dial:", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
-	defer c.Close()
+	w.Header().Set("Content-Type", "application/json")
+	//todo 往哪个连接发应解析业务逻辑
+	addr := p.Cmd
+	c := allconns[addr]
 
-	done := make(chan struct{})
+	err = c.WriteMessage(websocket.TextMessage, []byte("这是一条测试"))
+	if err != nil {
+		fmt.Println("节点消息投送失败")
+		panic(err)
 
-	go func() {
-		defer close(done)
-		for {
-			_, message, err := c.ReadMessage()
-			if err != nil {
-				log.Println("read:", err)
-				return
-			}
-			log.Printf("recv: %s", message)
-		}
-	}()
-
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-done:
-			return
-		case t := <-ticker.C:
-			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
-			if err != nil {
-				log.Println("write:", err)
-				return
-			}
-		case <-interrupt:
-			log.Println("interrupt")
-
-			// Cleanly close the connection by sending a close message and then
-			// waiting (with timeout) for the server to close the connection.
-			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-			if err != nil {
-				log.Println("write close:", err)
-				return
-			}
-			select {
-			case <-done:
-			case <-time.After(time.Second):
-			}
-			return
-		}
 	}
+	rv := rusult{
+		Code: 200,
+		Msg:  "消息已成功发送到执行节点",
+	}
+	err = json.NewEncoder(w).Encode(rv)
+	if err != nil {
+		//... handle error
+		panic(err)
+	}
+
 }
--- main/main.go
+++ main/main.go
@@ -11,9 +11,11 @@ import (
 func main() {
 	var flags struct {
 		Mode string
+		Addr string
 	}
 
 	flag.StringVar(&flags.Mode, "m", "h", "程序哪种模式运行d/h(打印版本)")
+	flag.StringVar(&flags.Addr, "addr", "localhost:8080", "以h运行时,监听的地址和端口,默认 localhost:8080")
 	flag.Parse()
 
 	if flags.Mode == "d" {
@@ -23,7 +25,7 @@ func main() {
 
 	if flags.Mode == "h" {
 		fmt.Println("程序启动以节点模式运行")
-		avah.HLocal()
+		avah.HLocal(flags.Addr)
 
 	}
 
