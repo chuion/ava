d10ecbe96976d4ba21c5ce2c7d5f2fffbfa963a8
inksnw <inksnw@gmail.com>
2020-12-29 14:29:12 +0800

    删除多余的循环与判断

--- avad/rconnect.go
+++ avad/rconnect.go
@@ -17,27 +17,21 @@ func dialTcp(address string) {
 	host := strings.Split(address, ":")[0]
 	tcpStatus.Set(host, false)
 
-	status, _ := tcpStatus.Get(host)
-	if !status.(bool) {
-		for {
-			conn, err := net.Dial("tcp", address)
-			if err != nil {
-				log.Debug().Msgf("连接远端tcp通道 %s失败,%s后重试", address, core.PongWait)
-				break
-			}
-			tcpStatus.Set(host, true)
-			log.Debug().Msgf("已创建连接节点tcp反弹通道%s", address)
-
-			session, err = yamux.Server(conn, nil)
-			if err != nil {
-				//todo 这里的处理好像还有坑
-				session.Close()
-				panic(err)
-			}
-			relay(host, session, server)
-		}
-
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		log.Debug().Msgf("连接远端tcp通道 %s失败,%s后重试", address, core.PongWait)
+		return
 	}
+	tcpStatus.Set(host, true)
+	log.Debug().Msgf("已创建连接节点tcp反弹通道%s", address)
+
+	session, err = yamux.Server(conn, nil)
+	if err != nil {
+		//todo 这里的处理好像还有坑
+		session.Close()
+		panic(err)
+	}
+	relay(host, session, server)
 }
 
 func relay(host string, session *yamux.Session, server *socks5.Server) {
@@ -48,7 +42,7 @@ func relay(host string, session *yamux.Session, server *socks5.Server) {
 			tcpStatus.Set(host, false)
 			break
 		}
-		log.Debug().Msgf("代理转发通信 %s %s",stream.LocalAddr(),stream.RemoteAddr())
+		log.Debug().Msgf("代理转发通信 %s %s", stream.LocalAddr(), stream.RemoteAddr())
 		go func() {
 			err = server.ServeConn(stream)
 			if err != nil {
@@ -62,17 +56,15 @@ func relay(host string, session *yamux.Session, server *socks5.Server) {
 func dialWs(addr string) {
 	host := strings.Split(addr, ":")[0]
 	wsStatus.Set(host, false)
-	status, _ := wsStatus.Get(host)
-	if !status.(bool) {
-		u := url.URL{Scheme: "ws", Host: addr, Path: "/ws"}
-		c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-		if err != nil {
-			log.Debug().Msgf("尝试连接节点ws通道%s失败,%s后重试:\n", addr, core.PongWait)
-			return
-		}
-		wsStatus.Set(host, true)
-		wsConns.Set(host, c)
-		log.Debug().Msgf("已连接节点ws通道%s\n", addr)
-		go getNodeInfo(host, c)
+
+	u := url.URL{Scheme: "ws", Host: addr, Path: "/ws"}
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Debug().Msgf("尝试连接节点ws通道%s失败,%s后重试:\n", addr, core.PongWait)
+		return
 	}
+	wsStatus.Set(host, true)
+	wsConns.Set(host, c)
+	log.Debug().Msgf("已连接节点ws通道%s\n", addr)
+	go getNodeInfo(host, c)
 }
